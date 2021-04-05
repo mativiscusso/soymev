6f17c52365e0d28fa212a78c558724b66f82c13d
mativiscusso <mativiscusso@gmail.com>
2021-04-05 18:22:43 -0300

    about page in mobile version

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import Layout from "../layout/Layout";
 import TitlePage from "../components/TitlePage";
 import matiImage from '../assets/svg/mativiscusso.svg'
+import iconsFront from '../assets/svg/Icons-front.svg'
+import iconsBack from '../assets/svg/Icons-back.svg'
+import iconsApi from '../assets/svg/Icons-api.svg'
+import iconsDesign from '../assets/svg/Icons-design.svg'
 
 const About = () => {
     return (
@@ -19,12 +23,14 @@ const About = () => {
                             facilitar los procesos y acciones. ¿Cómo?
                             Sistematizando y digitalizándolo todo.
                         </p>
+                        <br/>
                         <p>
                             Docente de la carrera Full Stack Node en el
                             instituto Digital House quienes me brindan la
                             posibilidad de desarrollarme dia a dia en este
                             fantastico mundo de aprender-enseñar.
                         </p>
+                        <br/>
                         <p>
                             Es el inicio de una nueva era, donde prevalecerán,
                             quienes se unan a la tecnología.{" "}
@@ -35,12 +41,16 @@ const About = () => {
                         <div className="enviroment">
                             <h4 className="subtitle">front-end</h4>
                             <p>HTML - CSS - SCSS - JavaScript - React</p>
-                            <div className="brands"></div>
+                            <div className="brands">
+                                <img src={iconsFront} alt="icons front end"/>
+                            </div>
                         </div>
                         <div className="enviroment">
                             <h4 className="subtitle">api</h4>
                             <p>REST - GRAPHQL</p>
-                            <div className="brands"></div>
+                            <div className="brands">
+                            <img src={iconsApi} alt="icons api"/>
+                            </div>
                         </div>
                         <div className="enviroment">
                             <h4 className="subtitle">back-end</h4>
@@ -48,12 +58,16 @@ const About = () => {
                                 Node JS - Express JS - Typescript - MySQL -
                                 MongoDB
                             </p>
-                            <div className="brands"></div>
+                            <div className="brands">
+                            <img src={iconsBack} alt="icons back end"/>
+                            </div>
                         </div>
                         <div className="enviroment">
                             <h4 className="subtitle">diseño</h4>
                             <p>Photoshop - illustrator - Figma</p>{" "}
-                            <div className="brands"></div>
+                            <div className="brands">
+                                <img src={iconsDesign} alt="icons design"/>
+                            </div>
                         </div>
                     </div>
                 </div>
