68a1cbf661627e24f762bfc1b1388b80dfe503e6
mativiscusso <mativiscusso@gmail.com>
2021-04-07 11:22:53 -0300

    change position in directory the index.scss file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './Routes';
-import './assets/styles/index.scss'
+import './index.scss'
 
 ReactDOM.render(
   <React.StrictMode>
