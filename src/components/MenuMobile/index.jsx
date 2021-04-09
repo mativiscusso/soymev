b3d4a2db1ee8ee0242990c27c3927bd30e396e36
mativiscusso <mativiscusso@gmail.com>
2021-04-09 13:22:45 -0300

    add social medias in menu mobile

--- src/components/MenuMobile/index.jsx
+++ src/components/MenuMobile/index.jsx
@@ -1,25 +1,57 @@
 import React from "react";
 import { push as Menu } from "react-burger-menu";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import logoLinkdin from "../../assets/svg/linkedin.png";
+import logoGithub from "../../assets/svg/github.png";
 
 const index = (props) => {
     return (
         <Menu {...props}>
-            <Link className="bm-item" to="/">
-                home
-            </Link>
-
-            <Link className="bm-item" to="/about">
+            <NavLink
+                activeClassName="navbar-links-active"
+                className="bm-item"
+                to="/about"
+            >
                 sobre mi
-            </Link>
+            </NavLink>
 
-            <Link className="bm-item" to="/portfolio">
+            <NavLink
+                activeClassName="navbar-links-active"
+                className="bm-item"
+                to="/portfolio"
+            >
                 portfolio
-            </Link>
+            </NavLink>
 
-            <Link className="bm-item" to="/contact">
+            <NavLink
+                activeClassName="navbar-links-active"
+                className="bm-item"
+                to="/contact"
+            >
                 contacto
-            </Link>
+            </NavLink>
+            <div className="d-flex">
+                <li className="hvr-underline-from-center">
+                    <a
+                        href="https://www.linkedin.com/in/mati-viscusso/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="pr-3"
+                    >
+                        <img src={logoLinkdin} alt="Linkedin Mati Viscusso" />
+                    </a>
+                </li>
+                <li className="hvr-underline-from-center">
+                    <a
+                        href="https://github.com/mativiscusso"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="pr-3"
+                    >
+                        <img src={logoGithub} alt="Github Mati Viscusso" />
+                    </a>
+                </li>
+            </div>
         </Menu>
     );
 };
