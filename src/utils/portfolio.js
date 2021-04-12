ec9be052db224c309355420aa714187c666b367b
mativiscusso <mativiscusso@gmail.com>
2021-04-12 13:02:41 -0300

    fix url in portfolio object

--- src/utils/portfolio.js
+++ src/utils/portfolio.js
@@ -1,73 +1,73 @@
 const portolio = [
     {
         name: "E-commerce MEV",
-        url: "ecommerce-starter-mev.herokuapp.com/",
+        url: "http://ecommerce-starter-mev.herokuapp.com/",
         image:"ecommerce-mev.png",
         technologies:"Node JS, Express, EJS"
     },
     {
         name: "Dashboard E-commerce",
-        url: "ecommerce-starter-mev.herokuapp.com/admin",
+        url: "http://ecommerce-starter-mev.herokuapp.com/admin",
         image:"dashboard-mev.png",
         technologies:"Node JS, Express, EJS"
     },
     {
         name: "IAN Digital Talent",
-        url: "www.iandigitaltalent.com",
+        url: "https://www.iandigitaltalent.com",
         image:"acheyl.png",
         technologies:"HTML, SCSS, JavaScript"
     },
     {
         name: "Somos marea",
-        url: "www.somosmarea.com",
+        url: "https://www.somosmarea.com",
         image:"marea.png",
         technologies:"HTML, CSS, JavaScript"
     },
     {
         name: "UVC Soluciones",
-        url: "www.uvcsoluciones.com",
+        url: "https://www.uvcsoluciones.com",
         image:"uvc.png",
         technologies:"HTML, SCSS, JavaScript"
     },
     {
         name: "Keclon",
-        url: "www.keclon.com",
+        url: "http://www.keclon.com",
         image:"keclon.png",
         technologies:"HTML, SCSS"
     },
     {
         name: "Asesor contable online",
-        url: "www.asesorcontableonline.com",
+        url: "https://www.asesorcontableonline.com",
         image:"asesor.png",
         technologies:"HTML, CSS, JavaScript, PHP"
     },
     {
         name: "Maximo limpieza",
-        url: "www.maximolimpieza.com",
+        url: "https://www.maximolimpieza.com",
         image:"maximo.png",
         technologies:"Wordpress"
     },
     {
         name: "Ache & L",
-        url: "www.acheyl.com.ar",
+        url: "http://www.acheyl.com.ar",
         image:"acheyl.png",
         technologies:"HTML, CSS, JavaScript"
     },
     {
         name: "Aromax S.A",
-        url: "www.aromaxsa.com.ar",
+        url: "https://www.aromaxsa.com.ar",
         image:"aromaxsa.png",
         technologies:"HTML, SCSS, JavaScript"
     },
     {
         name: "Consultorios Belgrano",
-        url: "www.consultoriobelgrano.com.ar",
+        url: "https://www.consultoriobelgrano.com.ar",
         image:"belgrano.png",
         technologies:"HTML, CSS, JavaScript"
     },
     {
         name: "Moderador Repuestos Ya",
-        url: "api.repuestosya.net",
+        url: "http://api.repuestosya.net",
         image:"repuestosya.png",
         technologies:"React"
     },
