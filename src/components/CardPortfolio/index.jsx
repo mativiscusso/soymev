09543198483f6052788d697205df193161ada985
mativiscusso <mativiscusso@gmail.com>
2021-04-12 16:21:31 -0300

    add icon github in footer card portfolio

--- src/components/CardPortfolio/index.jsx
+++ src/components/CardPortfolio/index.jsx
@@ -1,16 +1,30 @@
-import React from 'react';
+import React from "react";
+import iconGithub from "../../assets/svg/github.png";
 
-const CardPortfolio = ({title, url, image}) => {
-    const urlImage = `${process.env.REACT_APP_URL}/assets/img/${image}`
+const CardPortfolio = ({ title, url, image, repository }) => {
+    const urlImage = `${process.env.REACT_APP_URL}/assets/img/${image}`;
     return (
         <>
-            <img className="card-portfolio-image" src={urlImage} alt="portfolio"/>
+            <img
+                className="card-portfolio-image"
+                src={urlImage}
+                alt="portfolio"
+            />
             <div className="card-portfolio-footer">
                 <h5>{title}</h5>
-                <a href={url} target="_blank" rel="noopener noreferrer">VISITAR SITIO ▶</a>
+                <a href={url} target="_blank" rel="noopener noreferrer">
+                    VISITAR SITIO ▶
+                </a>
+                <a href={repository} target="_blank" rel="noopener noreferrer">
+                    <img
+                        className="card-portfolio-footer-icon"
+                        src={iconGithub}
+                        alt="icon github"
+                    />
+                </a>
             </div>
         </>
     );
-}
+};
 
 export default CardPortfolio;
